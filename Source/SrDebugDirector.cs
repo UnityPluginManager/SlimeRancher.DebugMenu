b22cf3db4c00b09a61eeed897514ec184f90756b
DaZombieKiller <bennyboy.private@hotmail.com.au>
2017-11-28 04:50:32 +1000

    Initial commit

---
+++ Source/SrDebugDirector.cs
@@ -0,0 +1,269 @@
+using System;
+using UnityEngine;
+using PluginManager.Core;
+
+/// <summary>The restored Debug Director</summary>
+[OnGameInit]
+public class SrDebugDirector : MonoBehaviour
+{
+    // Directors
+    // --------------------------------------------------------------------------------
+    private TimeDirector _timeDir;
+    private PediaDirector _pediaDir;
+    private TutorialDirector _tutorialDir;
+    private AchievementsDirector _achieveDir;
+    private ProgressDirector _progressDir;
+    private PlayerState _playerState;
+    private AutoSaveDirector _autoSave;
+    private InputDirector _inputDir;
+
+    // Debug Settings
+    // --------------------------------------------------------------------------------
+    private static bool _debug = true;
+    private static bool _showHelp = true;
+    
+    private bool _noclip;
+    private bool _infiniteEnergy;
+    private bool _infiniteHealth;
+    
+    // Object & Component References
+    // --------------------------------------------------------------------------------
+    private vp_FPController _fpController;
+    private GameObject _player;
+    private GameObject _camera;
+    private GameObject _hudUi;
+    
+    // Miscellaneous
+    // --------------------------------------------------------------------------------
+    private Vector3 _noclipPos;
+    private Font _helpFont;
+
+    /// <summary>Called on plugin init</summary>
+    private void Awake()
+    {
+        // set the default help menu font
+        _helpFont = Font.CreateDynamicFontFromOSFont("Consolas", 18);
+    }
+
+    /// <summary>Handles debugging features</summary>
+    private void Update()
+    {
+        // retrieve the directors
+        if (!_timeDir) _timeDir = FindObjectOfType<TimeDirector>();
+        if (!_pediaDir) _pediaDir = FindObjectOfType<PediaDirector>();
+        if (!_tutorialDir) _tutorialDir = FindObjectOfType<TutorialDirector>();
+        if (!_achieveDir) _achieveDir = FindObjectOfType<AchievementsDirector>();
+        if (!_progressDir) _progressDir = FindObjectOfType<ProgressDirector>();
+        if (!_playerState) _playerState = FindObjectOfType<PlayerState>();
+        if (!_autoSave) _autoSave = FindObjectOfType<AutoSaveDirector>();
+        if (!_inputDir)
+        {
+            _inputDir = FindObjectOfType<InputDirector>();
+            if (_inputDir) // prevent user from submitting bug reports.
+            {
+                _inputDir.bugReportPrefab = new GameObject();
+                _inputDir.bugReportPrefab.SetActive(false);
+            }
+        }
+        
+        // retrieve object and component references
+        if (!_player) _player = GameObject.Find("SimplePlayer");
+        if (!_fpController) _fpController = _player?.GetComponent<vp_FPController>();
+        if (!_camera) _camera = GameObject.Find("FPSCamera");
+        if (!_hudUi) _hudUi = GameObject.Find("HudUI");
+        
+        // the debug menu shouldn't function if the game is frozen
+        if (Time.timeScale <= 0f) return;
+        
+        // toggle the debug menu
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            _debug = !_debug;
+            
+            // if the debug menu was on previously,
+            // turn off any cheats that may be on.
+            if (!_debug)
+            {
+                _noclip = false;
+                _infiniteEnergy = false;
+                _infiniteHealth = false;
+                
+                // make hud visible
+                _hudUi?.SetActive(true);
+            }
+        }
+
+        // debug mode is off, nothing more to do here
+        if (!_debug)
+            return;
+
+        // decrement time of day
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            _timeDir?.AdjustTimeOfDay(-0.0416666679f);
+            
+            // check for negative time
+            if (_timeDir?.WorldTime() <= 0f)
+                _timeDir?.SetWorldTime(0f);
+        }
+
+        // increment time of day
+        else if (Input.GetKeyDown(KeyCode.RightBracket))
+            _timeDir?.AdjustTimeOfDay(0.0416666679f);
+
+        // toggle help menu
+        if (Input.GetKeyDown(KeyCode.F10))
+            _showHelp = !_showHelp;
+
+        // add 1000 credits
+        if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadPlus))
+            _playerState?.AddCurrency(1000);
+        
+        // subtract 1000 credits
+        else if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus))
+            _playerState?.AddCurrency(-1000);
+
+        // toggle heads-up display
+        if (Input.GetKeyDown(KeyCode.F6))
+            _hudUi?.SetActive(!_hudUi.activeSelf);
+
+        // reset player progress
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            Console.WriteLine("DEBUG: Tutorials/Pedia/Achievements/Progress Reset");
+            _pediaDir?.DebugClearUnlocked();
+            _tutorialDir?.DebugClearCompleted();
+            _achieveDir?.DebugClearAwarded();
+            _progressDir?.DebugClearProgress();
+        }
+        
+        // unlock all progress
+        else if (Input.GetKeyDown(KeyCode.F11))
+        {
+            Console.WriteLine("DEBUG: Tutorials/Pedia/Achievements/Progress Unlocked");
+            _pediaDir?.DebugAllUnlocked();
+            _tutorialDir?.DebugAllCompleted();
+            _achieveDir?.DebugAllAwarded();
+            _progressDir?.DebugUnlockProgress();
+        }
+
+        // unlock all upgrades
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Console.WriteLine("DEBUG: All Personal Upgrades");
+            _playerState?.DebugGiveAllUpgrades();
+            _playerState?.SetHealth(_playerState.GetMaxHealth());
+            _playerState?.SetEnergy(_playerState.GetMaxEnergy());
+            
+            // add 5 keys
+            for (var i = 0; i < 5; i++)
+                _playerState?.AddKey();
+        }
+
+        // fill inventory with random items and ammo
+        if (Input.GetKeyDown(KeyCode.Alpha9) && _playerState)
+        {
+            Console.WriteLine("DEBUG: Fill Ammo");
+            _playerState?.Ammo?.DebugFillRandomAmmo(_playerState.GetMaxAmmo());
+        }
+
+        // toggle infinite energy
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            _infiniteEnergy = !_infiniteEnergy;
+        
+        // toggle infinite health
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+            _infiniteHealth = !_infiniteHealth;
+        
+        // clear inventory
+        if (Input.GetKeyDown(KeyCode.K))
+            _playerState?.Ammo?.Clear();
+        
+        // refill inventory
+        if (Input.GetKeyDown(KeyCode.L) && _playerState)
+            _playerState?.Ammo?.DebugRefillAmmo(_playerState.GetMaxAmmo());
+        
+        // toggle noclip
+        if (Input.GetKeyDown(KeyCode.N) && _player)
+        {
+            _noclip = !_noclip;
+            _noclipPos = _player.transform.position;
+        }
+
+        // force the game to save
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            Console.WriteLine("DEBUG: Forcing save now");
+            _autoSave?.SaveAllNow();
+        }
+
+        // handle noclip, if it's enabled
+        if (_noclip && _camera && _fpController)
+        {
+            // calculate speed, will be multiplied by two if run is held
+            var speed = 20f * (SRInput.Actions.run.State ? 2f : 1f);
+            
+            // add movement
+            _noclipPos += _camera.transform.forward * SRInput.Actions.vertical.RawValue * speed * Time.deltaTime;
+            _noclipPos += _camera.transform.right * SRInput.Actions.horizontal.RawValue * speed * Time.deltaTime;
+
+            // stop all movement on the controller and reposition it
+            _fpController?.Stop();
+            _fpController?.SetPosition(_noclipPos);
+        }
+
+        if (_infiniteEnergy && _playerState) // infinite energy's on, set our energy to max
+            _playerState.SetEnergy(_playerState.GetMaxEnergy());
+        
+        if (_infiniteHealth && _playerState) // god mode's on, set our health to max
+            _playerState.SetHealth(_playerState.GetMaxHealth());
+    }
+    
+    /// <summary>Displays the help menu</summary>
+    private void OnGUI()
+    {
+        if (!_showHelp)
+            return;
+
+        GUI.skin.label.font = _helpFont;
+        GUI.skin.label.alignment = TextAnchor.LowerRight;
+
+        // construct the help menu text
+        var helpMessage =
+           $"<b>DEBUG MODE IS {(_debug ? "ENABLED" : "DISABLED")} \n\n" +
+           
+            "AWARD ALL PERSONAL UPGRADES     0 \n" +
+            "TOGGLE INFINITE ENERGY     6 \n" +
+            "TOGGLE INFINITE HEALTH     7 \n" +
+            "FORCE SAVE     8 \n" +
+            "RANDOM ITEMS/AMMO     9 \n\n" +
+           
+            "CLEAR INVENTORY     K \n" +
+            "REFILL INVENTORY     L \n" +
+            "TOGGLE NOCLIP     N \n\n" +
+           
+            "ADD 1000 CREDITS     + \n" +
+            "REMOVE 1000 CREDITS     - \n" +
+            "DECREMENT TIME OF DAY     [ \n" +
+            "INCREMENT TIME OF DAY     ] \n\n" +
+           
+            "TOGGLE HUD    F6 \n" +
+            "TOGGLE DEBUG MODE    F9 \n" +
+            "TOGGLE DEBUG HELP DISPLAY   F10 \n" +
+            "UNLOCK ALL PROGRESS   F11 \n" +
+            "RESET PROGRESS   F12 \n</b>";
+
+        // display outline for text
+        for (var x = -2; x <= 2; x += 2)
+        {
+            for (var y = -2; y <= 2; y += 2)
+            {
+                GUI.skin.label.normal.textColor = Color.black;
+                GUI.Label(new Rect(x, y, Screen.width, Screen.height), helpMessage);
+            }
+        }
+
+        GUI.skin.label.normal.textColor = Color.white;
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), helpMessage);
+    }
+}
---
+++ Source/SrDebugExtensions.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Debug functions that were removed from the game</summary>
+public static class SrDebugExtensions
+{
+    /// <summary>Retrieves a private field of type T2 from a class instance of type T1</summary>
+    /// <param name="fieldName">The name of the field to retrieve</param>
+    /// <param name="instance">The instance to retrieve the field from</param>
+    private static T2 GetPrivateField<T1, T2>(string fieldName, T1 instance)
+    {
+        var field = typeof(T1).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+        return (T2) field?.GetValue(instance);
+    }
+
+    // PediaDirector
+    // --------------------------------------------------------------------------------
+    
+    /// <summary>Clears unlocked pedia entries</summary>
+    /// <param name="self">The PediaDirector instance</param>
+    public static void DebugClearUnlocked(this PediaDirector self)
+    {
+        var unlock = typeof(PediaDirector).GetMethod("Unlock", BindingFlags.NonPublic | BindingFlags.Instance);
+        GetPrivateField<PediaDirector, HashSet<PediaDirector.Id>>("unlocked", self).Clear();
+        foreach (var t in self.initUnlocked)
+            unlock.Invoke(self, new object[] { t });
+    }
+
+    /// <summary>Unlocks all pedia entries</summary>
+    /// <param name="self">The PediaDirector instance</param>
+    public static void DebugAllUnlocked(this PediaDirector self)
+    {
+        var unlock = typeof(PediaDirector).GetMethod("Unlock", BindingFlags.NonPublic | BindingFlags.Instance);
+        foreach (PediaDirector.Id id in Enum.GetValues(typeof(PediaDirector.Id)))
+            unlock.Invoke(self, new object[] { id });
+    }
+
+    // TutorialDirector
+    // --------------------------------------------------------------------------------
+    
+    /// <summary>Clears all completed tutorials</summary>
+    /// <param name="self">The TutorialDirector instance</param>
+    public static void DebugClearCompleted(this TutorialDirector self)
+    {
+        GetPrivateField<TutorialDirector, HashSet<TutorialDirector.Id>>("completed", self).Clear();
+    }
+
+    /// <summary>Completes all tutorials</summary>
+    /// <param name="self">The TutorialDirector instance</param>
+    public static void DebugAllCompleted(this TutorialDirector self)
+    {
+        var completed = GetPrivateField<TutorialDirector, HashSet<TutorialDirector.Id>>("completed", self);
+        foreach (TutorialDirector.Id id in Enum.GetValues(typeof(TutorialDirector.Id))) completed.Add(id);
+    }
+
+    // AchievementsDirector
+    // --------------------------------------------------------------------------------
+    
+    /// <summary>Clears all awarded achievements</summary>
+    /// <param name="self">The AchievementsDirector instance</param>
+    public static void DebugClearAwarded(this AchievementsDirector self)
+    {
+        GetPrivateField<AchievementsDirector, HashSet<AchievementsDirector.Achievement>>("earnedAchievements", self).Clear();
+        GetPrivateField<AchievementsDirector, Dictionary<AchievementsDirector.BoolStat, bool>>("boolStatDict", self).Clear();
+        GetPrivateField<AchievementsDirector, Dictionary<AchievementsDirector.IntStat, int>>("intStatDict", self).Clear();
+        GetPrivateField<AchievementsDirector, Dictionary<AchievementsDirector.EnumStat, HashSet<Enum>>>("enumStatDict", self).Clear();
+        GetPrivateField<AchievementsDirector, Dictionary<AchievementsDirector.GameFloatStat, float>>("gameFloatStatDict", self).Clear();
+    }
+
+    /// <summary>Awards all achievements</summary>
+    /// <param name="self">The AchievementsDirector instance</param>
+    public static void DebugAllAwarded(this AchievementsDirector self)
+    {
+        var earnedAchievements = GetPrivateField<AchievementsDirector, HashSet<AchievementsDirector.Achievement>>("earnedAchievements", self);
+        foreach (AchievementsDirector.Achievement achievement in Enum.GetValues(typeof(AchievementsDirector.Achievement))) earnedAchievements.Add(achievement);
+    }
+
+    // ProgressDirector
+    // --------------------------------------------------------------------------------
+    
+    /// <summary>Clears all progress</summary>
+    /// <param name="self">The ProgressDirector instance</param>
+    public static void DebugClearProgress(this ProgressDirector self)
+    {
+        GetPrivateField<ProgressDirector, Dictionary<ProgressDirector.ProgressType, int>>("progressDict", self).Clear();
+    }
+
+    /// <summary>Unlocks all progress</summary>
+    /// <param name="self">The ProgressDirector instance</param>
+    public static void DebugUnlockProgress(this ProgressDirector self)
+    {
+        foreach (ProgressDirector.ProgressType type in
+            Enum.GetValues(typeof(ProgressDirector.ProgressType)))
+        {
+            // ReSharper disable once SwitchStatementMissingSomeCases
+            switch (type)
+            {
+                case ProgressDirector.ProgressType.CORPORATE_PARTNER:
+                    self.SetProgress(type, 28);
+                    break;
+                default:
+                    self.SetProgress(type, 1);
+                    break;
+            }
+        }
+    }
+
+    // PlayerState
+    // --------------------------------------------------------------------------------
+    
+    /// <summary>Gives the player all upgrades</summary>
+    /// <param name="self">The PlayerState instance</param>
+    public static void DebugGiveAllUpgrades(this PlayerState self)
+    {
+        foreach (PlayerState.Upgrade upgrade in Enum.GetValues(typeof(PlayerState.Upgrade)))
+            self.AddUpgrade(upgrade);
+    }
+
+    // Ammo
+    // --------------------------------------------------------------------------------
+    
+    /// <summary>Fills the player's inventory with random items</summary>
+    /// <param name="self">The Ammo instance</param>
+    /// <param name="fillTo"></param>
+    public static void DebugFillRandomAmmo(this Ammo self, int fillTo)
+    {
+        var potentialAmmo = GetPrivateField<Ammo, GameObject[]>("potentialAmmo", self);
+        var numSlots = GetPrivateField<Ammo, int>("numSlots", self);
+
+        var ammoSlot = typeof(Ammo).GetNestedType("Slot", BindingFlags.NonPublic | BindingFlags.Instance);
+        var slots = GetPrivateField<Ammo, object[]>("slots", self);
+        var emotions = ammoSlot.GetField("emotions", BindingFlags.Public | BindingFlags.Instance);
+
+        for (var i = 0; i < numSlots; i++)
+        {
+            // pick a random item to insert into the slot
+            var plucked = Randoms.SHARED.Pluck(new List<GameObject>(potentialAmmo), null);
+            
+            // instantiate ammo slot
+            var constructor = ammoSlot.GetConstructors()[0];
+            var parameters = constructor.GetParameters();
+            if (parameters[0].ParameterType == typeof(GameObject))
+                slots[i] = Activator.CreateInstance(ammoSlot, plucked, fillTo);
+            else if (parameters[0].ParameterType == typeof(Identifiable.Id)) // 0.6.0+
+                slots[i] = Activator.CreateInstance(ammoSlot, plucked.GetComponent<Identifiable>().id, fillTo);
+            
+            if (!Identifiable.IsSlime(plucked.GetComponent<Identifiable>().id)) continue;
+            var emotionData = new SlimeEmotionData
+            {
+                [SlimeEmotions.Emotion.AGITATION] = 0,
+                [SlimeEmotions.Emotion.HUNGER] = .5f,
+                [SlimeEmotions.Emotion.FEAR] = 0,
+            };
+            emotions?.SetValue(slots[i], emotionData);
+        }
+    }
+
+    /// <summary>Refills the player's inventory</summary>
+    /// <param name="self">The Ammo instance</param>
+    /// <param name="fillTo"></param>
+    public static void DebugRefillAmmo(this Ammo self, int fillTo)
+    {
+        var ammoSlot = typeof(Ammo).GetNestedType("Slot", BindingFlags.NonPublic | BindingFlags.Instance);
+        var slotCount = ammoSlot.GetField("count");
+        var numSlots = GetPrivateField<Ammo, int>("numSlots", self);
+        var slots = GetPrivateField<Ammo, object[]>("slots", self);
+
+        for (var i = 0; i < numSlots; i++)
+        {
+            if (slots[i] != null)
+                slotCount?.SetValue(slots[i], fillTo);
+            else if (i == numSlots - 1) // refill water if slot empty
+                slots[i] = Activator.CreateInstance(ammoSlot, Identifiable.Id.WATER_LIQUID, fillTo);
+        }
+    }
+}
