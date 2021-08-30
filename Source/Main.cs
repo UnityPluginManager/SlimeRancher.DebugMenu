3ee7148f631a8a7e7e86208b47604fc377ed74dc
KomiksPL <44307026+KomiksPL@users.noreply.github.com>
2021-08-30 17:27:10 +0200

    Main and modinfo for SRML

---
+++ Source/Main.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Reflection;
+using SRML;
+using SRML.SR;
+
+namespace SrDebugDirector
+{
+    public class Main : ModEntryPoint
+    {
+        public static Assembly execAssembly = Assembly.GetExecutingAssembly();
+
+        public override void PreLoad()
+        {
+            HarmonyInstance.PatchAll(execAssembly);
+        }
+
+        // Token: 0x06000002 RID: 2 RVA: 0x0000208C File Offset: 0x0000028C
+        public override void Load()
+        {
+            SRCallbacks.OnSaveGameLoaded += context => SRSingleton<SceneContext>.Instance.Player.AddComponent<SrDebugDirector>();
+        }
+		
+    }
+}
