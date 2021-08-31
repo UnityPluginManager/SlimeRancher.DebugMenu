ee9980bd0304a885a0578339931a03dbc8a30e5a
KomiksPL <44307026+KomiksPL@users.noreply.github.com>
2021-08-31 10:07:41 +0200

    Update SrDebugHarmony.cs

--- Source/SrDebugHarmony.cs
+++ Source/SrDebugHarmony.cs
@@ -3,12 +3,10 @@ using MonomiPark.SlimeRancher.DataModel;
 
 namespace SrDebugDirector
 {
-    // Token: 0x02000004 RID: 4
     [HarmonyPatch(typeof(PediaModel))]
     [HarmonyPatch("Init")]
     internal static class PediaModelInstance
     {
-        // Token: 0x06000007 RID: 7 RVA: 0x00002080 File Offset: 0x00000280
         private static void Postfix(PediaModel __instance)
         {
             pediamodel = __instance;
@@ -19,12 +17,10 @@ namespace SrDebugDirector
 }
 namespace SrDebugDirector
 {
-    // Token: 0x02000004 RID: 4
     [HarmonyPatch(typeof(TutorialsModel))]
     [HarmonyPatch("Init")]
     internal static class TutorialsModelInstance
     {
-        // Token: 0x06000007 RID: 7 RVA: 0x00002080 File Offset: 0x00000280
         private static void Postfix(TutorialsModel __instance)
         { 
             tutorialsmodel = __instance;
@@ -35,12 +31,10 @@ namespace SrDebugDirector
 }
 namespace SrDebugDirector
 {
-    // Token: 0x02000004 RID: 4
     [HarmonyPatch(typeof(ProfileAchievesModel))]
     [HarmonyPatch("Init")]
     internal static class ProfileAchievesModelInstance
     {
-        // Token: 0x06000007 RID: 7 RVA: 0x00002080 File Offset: 0x00000280
         private static void Postfix(ProfileAchievesModel __instance)
         {
             profileachievesmodel = __instance;
@@ -48,28 +42,19 @@ namespace SrDebugDirector
         }
 
         public static ProfileAchievesModel profileachievesmodel;
-
-
-
     }
 }
 namespace SrDebugDirector
 {
-    // Token: 0x02000004 RID: 4
     [HarmonyPatch(typeof(ProgressModel))]
     [HarmonyPatch("Init")]
     internal static class ProgressModelInstance
     {
-        // Token: 0x06000007 RID: 7 RVA: 0x00002080 File Offset: 0x00000280
         private static void Postfix(ProgressModel __instance)
         {
             progressmodel = __instance;
         }
         
         public static ProgressModel progressmodel;
-
-
-
-
     }
 }
